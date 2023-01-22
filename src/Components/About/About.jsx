e1d74e16933aeeec50ad4ab7d8ccb628f49062e7
santiagogozu <santiagogo1993@gmail.com>
2023-01-22 15:20:03 -0500

    Adding About

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import './About.css'
+import React from "react";
+import "./About.css";
+import imgAbout from "../../img/wallpaperCode.jpg";
 // import downloadResume from '../../../src/img/img_btn_icon.png'
 // import resume from '../../img/Resume of MD. Rafiul Alam.pdf'
 const About = () => {
   return (
     <section id="about">
-      <div >
-        <h2 id="hello_header">Hi_</h2>
-        <div className="section__description">
-          <p className="section__description">
-            I am Santiago, a Full Stack Web Developer with knowledge in HTML5, CSS, Javascript, React, Redux, NodeJs, SQL, Sequelize.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, similique atque non officia, magni perspiciatis explicabo minima reprehenderit id aperiam error in eveniet est consequuntur vitae voluptatem qui nesciunt autem.
-          </p>
+      <div className="about">
+        <div className="imgAbout">
+          <img src={imgAbout} alt="" />
+        </div>
+        <div className="containerAbout">
+          <h2 id="hello_header">Hi_</h2>
+          <div className="section__description">
+            <p className="section__description">
+              I am Santiago Gonzalez, a junior developer able to build a Web
+              presence from the ground up - from concept, navigation and layout.
+              Skilled in Front End technologies such as Html5 , Css3 ,
+              JavaScript , React ,Angular, Sass, and Back End
+              technologies such as Node.JS, Express JS and MongoDb. Learning a
+              variety of scripting languages and multimedia tools is one of my
+              strengths. I am also a hard worker and team player. I am looking
+              for a challenging opportunity that will enable me to use my skills
+              and abilities to achieve a challenging goal.
+            </p>
+          </div>
         </div>
         {/* <a href={resume} target="_blank" className="section_btn site-btn" data-aos="zoom-in"
           data-aos-easing="linear"
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,20 +1,28 @@
-import React, { useRef } from 'react';
-import './Contact.css';
-import { Typewriter } from 'react-simple-typewriter';
-import emailjs from 'emailjs-com';
+import React, {useRef} from "react";
+import "./Contact.css";
+import {Typewriter} from "react-simple-typewriter";
+import emailjs from "emailjs-com";
 // import ScrollToTop from "react-scroll-to-top";
 const Contact = () => {
-
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_zwf25k2', 'template_c75ssrd', form.current, 'TKRw6R4ZAQ92fmzbG')
-      .then(() => {
-        window.alert("Correo enviado correctamente");
-      }, (error) => {
-        window.alert(error.text);
-      });
+    emailjs
+      .sendForm(
+        "service_zwf25k2",
+        "template_c75ssrd",
+        form.current,
+        "TKRw6R4ZAQ92fmzbG"
+      )
+      .then(
+        () => {
+          window.alert("Correo enviado correctamente");
+        },
+        (error) => {
+          window.alert(error.text);
+        }
+      );
   };
 
   return (
@@ -23,34 +31,67 @@ const Contact = () => {
       <div id="contact" className="container section">
         <div className="row">
           <div className="contacts_header">
-              <Typewriter
-                words={['Say Hi 👋', 'Get in touch now 🤗', "Let's have a cup of tea ☕️"]}
-                loop={true}
-                cursor
-                cursorStyle='_'
-                typeSpeed={50}
-                deleteSpeed={20}
-                delaySpeed={1000}
-              />
+            <Typewriter
+              words={[
+                "Say Hi 👋",
+                "Get in touch now 🤗",
+                "Let's have a cup of tea ☕️",
+              ]}
+              loop={true}
+              cursor
+              cursorStyle="_"
+              typeSpeed={50}
+              deleteSpeed={20}
+              delaySpeed={1000}
+            />
           </div>
         </div>
         <div className="containerEmail">
-            <div className='container'>
-                <p >Just write me a letter here_</p>
-                <form ref={form} onSubmit={sendEmail}>
-                    <input className='FormInput' type="text" placeholder="Your name" required name="name" id="validationCustom01" />
-                    <span></span>
-                    <input className='FormInput' type="email" placeholder="Your e-mail" required name="email" id="validationCustom02" />
-                    <span ></span>
-                    <div >
-                    <textarea className='FormTextArea' placeholder="Type the message here" required name="message" id="validationCustom03"></textarea>
-                    <span ></span>
-                    </div>
-                    <button  type="submit" value="Send">Send</button>
-                </form>
-                </div>
-            </div>
+          <div className="container">
+            <p>Just write me a letter here_</p>
+            <form ref={form} onSubmit={sendEmail}>
+              <input
+                className="FormInput"
+                type="text"
+                placeholder="Your name"
+                required
+                name="name"
+                id="validationCustom01"
+              />
+              <span></span>
+              <input
+                className="FormInput"
+                type="email"
+                placeholder="Your e-mail"
+                required
+                name="email"
+                id="validationCustom02"
+              />
+              <span></span>
+              <div>
+                <textarea
+                  className="FormTextArea"
+                  placeholder="Type the message here"
+                  required
+                  name="message"
+                  id="validationCustom03"
+                ></textarea>
+                <span></span>
+              </div>
+              <button
+                className="site-btn site-btn--form"
+                type="submit"
+                value="Send"
+              >
+                Send
+              </button>
+            </form>
           </div>
+        </div>
+      </div>
+      <div className="footer">
+        <p>@copy; 2023 Santiago Gonzalez. All Rights Reserved</p>
+      </div>
     </div>
   );
 };
